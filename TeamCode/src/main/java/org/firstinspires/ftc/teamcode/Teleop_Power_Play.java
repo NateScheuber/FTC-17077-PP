d7104fba3c83412e1770a171e5b8d3b2df79c75a
Nate Scheuber <13628@adnaschools.org>
2022-09-29 19:52:02 -0700

    First commit to just update gradle, also added Teleop_Power_Play and ftcdashboard

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_Power_Play.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Teleop_Power_Play extends LinearOpMode {
+    
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        while(opModeIsActive()){
+
+        }
+
+    }
+}
